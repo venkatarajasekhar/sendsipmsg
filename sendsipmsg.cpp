a2cbbf1a47d9268d1648a6460bd592071ff72988
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-10-02 16:50:21 +0530

    sendsipmsg.cpp

--- sendsipmsg.cpp
+++ sendsipmsg.cpp
@@ -1,24 +1,24 @@
 
-#include "rutil/Log.hxx"
-#include "rutil/Logger.hxx"
-#include "rutil/Subsystem.hxx"
-#include "resip/dum/ClientAuthManager.hxx"
-#include "resip/dum/ClientRegistration.hxx"
-#include "resip/dum/DialogUsageManager.hxx"
-#include "resip/dum/InviteSessionHandler.hxx"
-#include "resip/dum/MasterProfile.hxx"
-#include "resip/dum/Profile.hxx"
-#include "resip/dum/UserProfile.hxx"
-#include "resip/dum/RegistrationHandler.hxx"
-#include "resip/dum/ClientPagerMessage.hxx"
-#include "resip/dum/ServerPagerMessage.hxx"
+#include "rutil/Log.hpp"
+#include "rutil/Logger.hpp"
+#include "rutil/Subsystem.hpp"
+#include "resip/dum/ClientAuthManager.hpp"
+#include "resip/dum/ClientRegistration.hpp"
+#include "resip/dum/DialogUsageManager.hpp"
+#include "resip/dum/InviteSessionHandler.hpp"
+#include "resip/dum/MasterProfile.hpp"
+#include "resip/dum/Profile.hpp"
+#include "resip/dum/UserProfile.hpp"
+#include "resip/dum/RegistrationHandler.hpp"
+#include "resip/dum/ClientPagerMessage.hpp"
+#include "resip/dum/ServerPagerMessage.hpp"
 
-#include "resip/dum/DialogUsageManager.hxx"
-#include "resip/dum/AppDialogSet.hxx"
-#include "resip/dum/AppDialog.hxx"
-#include "resip/dum/RegistrationHandler.hxx"
-#include "resip/dum/PagerMessageHandler.hxx"
-#include "resip/stack/PlainContents.hxx"
+#include "resip/dum/DialogUsageManager.hpp"
+#include "resip/dum/AppDialogSet.hpp"
+#include "resip/dum/AppDialog.hpp"
+#include "resip/dum/RegistrationHandler.hpp"
+#include "resip/dum/PagerMessageHandler.hpp"
+#include "resip/stack/PlainContents.hpp"
 
 #include <iostream>
 #include <string>
@@ -32,20 +32,29 @@ using namespace resip;
 class ClientMessageHandler : public ClientPagerMessageHandler {
 public:
    ClientMessageHandler()
-      : finished(false),
+      : try{
+         finished(false),
         successful(false)
+      }catch(...){
+         
+      }
    {
    };
 
-   virtual void onSuccess(ClientPagerMessageHandle, const SipMessage& status)
+   //virtual void onSuccess(ClientPagerMessageHandle, const SipMessage& status)
+   virtual void onSuccess(ClientPagerMessageHandle &CliPagerMsgHandleObj, const SipMessage& status)
    {
+      //Logic needs to be implement
+      
       InfoLog(<<"ClientMessageHandler::onSuccess\n");
       successful = true;
       finished = true;
    }
 
-   virtual void onFailure(ClientPagerMessageHandle, const SipMessage& status, std::auto_ptr<Contents> contents)
+   //virtual void onFailure(ClientPagerMessageHandle, const SipMessage& status, std::auto_ptr<Contents> contents)
+   virtual void onFailure(ClientPagerMessageHandle &CliPagerMsgHandleObj , const SipMessage& status, auto_ptr<Contents> contents)
    {
+      //Logic needs to be implement
       ErrLog(<<"ClientMessageHandler::onFailure\n");
       successful = false;
       finished = true;
@@ -67,12 +76,31 @@ int main(int argc, char *argv[])
       ErrLog(<< "usage: " << argv[0] << " sip:from user passwd realm sip:to [port]\n");
       return 1;
    }
-
+   try{
    string from(argv[1]);
+   }catch(...){
+      
+   }
+   try{
    string user(argv[2]);
+   }catch(...){
+      
+   }
+   try{
    string passwd(argv[3]);
+   }catch(...){
+      
+   }
+   try{
    string realm(argv[4]);
-   string to(argv[5]);
+   }catch(...){
+      
+   }
+   try{
+      string to(argv[5]);
+   }catch(...){
+      
+   }
    int port = 5060;
    if(argc == 7)
    {
@@ -83,20 +111,42 @@ int main(int argc, char *argv[])
 
    InfoLog(<< "log: from: " << from << ", to: " << to << ", port: " << port << "\n");
    InfoLog(<< "user: " << user << ", passwd: " << passwd << ", realm: " << realm << "\n");
-
-   SharedPtr<MasterProfile> profile(new MasterProfile);
-   auto_ptr<ClientAuthManager> clientAuth(new ClientAuthManager());
-
+   try{ 
+   <MasterProfile> profile(new MasterProfile);
+   }catch(...){
+      
+   }
+   try{
+      <ClientAuthManager> clientAuth(new ClientAuthManager());
+   }catch(...){
+      
+   }
+   try{
    SipStack clientStack;
+   }catch(...){
+      
+   }
+   try{
    DialogUsageManager clientDum(clientStack);
+   }catch(...){
+      
+   }
+   try{
    clientDum.addTransport(UDP, port);
+   }catch(...){
+      
+   }
    clientDum.setMasterProfile(profile);
 
    clientDum.setClientAuthManager(clientAuth);
    clientDum.getMasterProfile()->setDefaultRegistrationTime(70);
    clientDum.getMasterProfile()->addSupportedMethod(MESSAGE);
    clientDum.getMasterProfile()->addSupportedMimeType(MESSAGE, Mime("text", "plain"));
+   try{
    ClientMessageHandler *cmh = new ClientMessageHandler();
+   }catch(...){
+      
+   }
    clientDum.setClientPagerMessageHandler(cmh);
 
    NameAddr naFrom(from.c_str());
@@ -104,17 +154,29 @@ int main(int argc, char *argv[])
    profile->setDigestCredential(realm.c_str(), user.c_str(), passwd.c_str());
 
    InfoLog(<< "Sending MESSAGE\n");
+   try{
    NameAddr naTo(to.c_str());
-   ClientPagerMessageHandle cpmh = clientDum.makePagerMessage(naTo);
-
-   Data messageBody("Hello world!");
-   auto_ptr<Contents> content(new PlainContents(messageBody));
+   }catch(...){
+      
+   }
+   ClientPagerMessageHandle cpmh;   //ctor
+   clientDum.makePagerMessage(naTo);
+   try{
+   Data messageBody("Hello world!");  //ctor
+   }catch(...){
+      
+   }
+   <Contents> content(new PlainContents(messageBody));
    cpmh.get()->page(content);
 
    // Event loop - stack will invoke callbacks in our app
    while(!cmh->isFinished())
    {
+      try{
       clientStack.process(100);
+      }catch(...){
+         
+      }
       while(clientDum.process());
    }
 
